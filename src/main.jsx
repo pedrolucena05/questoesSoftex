e9331c58642c937664ee1de5775f57ba77314dbe
Pedro Lucena <44994733+pedrolucena05@users.noreply.github.com>
2023-11-23 13:31:07 -0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Contador from './components/contador-inteligente.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Contador />
+  </React.StrictMode>,
+)
